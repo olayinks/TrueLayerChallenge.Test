93d785a835fb52e3c61798e3d560204702687c8e
Moses A <mosesagbeniga@outlook.com>
2021-08-02 16:07:33 +0100

    add translation service test

--- TrueLayerAPITest.cs
+++ TrueLayerAPITest.cs
@@ -31,6 +31,16 @@ namespace TrueLayerChallenge.Test
             {"PokemonURL", "https://pokeapi.co/api/v2/pokemon-species/"},
             {"translationURL", "https://api.funtranslations.com/translate/"} 
         };
+        private string translatedText = @"{
+    ""success"": {
+        ""total"": 1
+    },
+    ""contents"": {
+        ""translated"": ""Mewtwo is a pokémon yond wast did create by genetic manipulation. However,  coequal though the scientific power of humans did create this pokémon’s corse,  they did fail to endow mewtwo with a compassionate heart."",
+        ""text"": ""MEWTWO is a POKéMON that was created\nby genetic manipulation.However, even though the scientific power of humans created this POKéMON’s body, they failed to endow MEWTWO with a compassionate heart."",
+        ""translation"": ""shakespeare""
+    }
+}";
         private IResponse _response;
         public  TrueLayerAPITest()
         {
@@ -42,6 +52,7 @@ namespace TrueLayerChallenge.Test
         [MemberData(nameof(PokemonTestData))]
         public async Task ReadPokemonAPI_OkResponseAsync(PokemonDTO pokemonDTO)
         {
+
             var response = new HttpResponseMessage
             {
                 StatusCode = HttpStatusCode.OK,
@@ -67,6 +78,94 @@ namespace TrueLayerChallenge.Test
             Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
                        
         }
+
+        [Theory]
+        [MemberData(nameof(PokemonTestData))]
+        public async Task ReadPokemonAPI_BadRequestResponseAsync(PokemonDTO pokemonDTO)
+        {
+
+            var response = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.NotFound,
+                Content = new StringContent(JsonConvert.SerializeObject(pokemonDTO)),
+            };
+            mockHttpHandler
+               .Protected()
+               .Setup<Task<HttpResponseMessage>>(
+                  "SendAsync",
+                  ItExpr.IsAny<HttpRequestMessage>(),
+                  ItExpr.IsAny<CancellationToken>())
+               .ReturnsAsync(response);
+            var httpClient = new HttpClient(mockHttpHandler.Object);
+            _response = new Response(httpClient);
+            _controller = new PokemonController(_logger, _response, _config);
+            var result = await _controller.Get("mewtwo");
+            var notFoundResult = result as ObjectResult;
+
+            // assert
+            //Assert.NotNull(notFoundResult);
+            //Assert.True(notFoundResult is NotFoundObjectResult);
+            Assert.Equal(StatusCodes.Status400BadRequest, notFoundResult.StatusCode);
+
+
+        }
+
+        [Theory]
+        [MemberData(nameof(PokemonTestData))]
+        public async Task TranslatePokemonAPI_TranslatedResponseAsync(PokemonDTO pokemonDTO)
+        {
+            
+            //Arrange for Pokemon Fetch
+            var response = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(JsonConvert.SerializeObject(pokemonDTO)),
+            };
+            mockHttpHandler
+               .Protected()
+               .Setup<Task<HttpResponseMessage>>(
+                  "SendAsync",
+                  ItExpr.IsAny<HttpRequestMessage>(),
+                  ItExpr.IsAny<CancellationToken>())
+               .ReturnsAsync(response);
+            var httpClient = new HttpClient(mockHttpHandler.Object);
+
+
+            //Act for Pokemon Fetch
+            _response = new Response(httpClient);
+            var pokemon = await _response.FetchPokemon($"{_config["PokemonURL"]}mewtwo");
+
+            //assert for pokemon fetch
+            Assert.NotNull(pokemon);
+            Assert.IsType<Pokemon>(pokemon);
+
+            //arrange for translation
+            response = new HttpResponseMessage
+            {
+                StatusCode = HttpStatusCode.OK,
+                Content = new StringContent(translatedText),
+            };
+            mockHttpHandler
+               .Protected()
+               .Setup<Task<HttpResponseMessage>>(
+                  "SendAsync",
+                  ItExpr.IsAny<HttpRequestMessage>(),
+                  ItExpr.IsAny<CancellationToken>())
+               .ReturnsAsync(response);
+             httpClient = new HttpClient(mockHttpHandler.Object);
+
+            //Act for Translation
+            _response = new Response(httpClient);
+            var pokemonTranslated = await _response.Translation($"{_config["translationURL"]}",pokemon);
+
+            string res = JsonConvert.DeserializeObject<dynamic>(translatedText).contents.translated;
+            // assert for translation
+            Assert.NotNull(pokemonTranslated);
+            Assert.Contains(res, pokemonTranslated.Description);
+
+
+        }
+
         public static IEnumerable<object[]> PokemonTestData()
         {
             var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "pokemon.json");
@@ -78,5 +177,6 @@ namespace TrueLayerChallenge.Test
             }
             
         }
+     
     }
 }
